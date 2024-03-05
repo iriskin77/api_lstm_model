397b1d7a1938d95cd7cf39d9961d2c23d350e710
iriskin77 <talcore@yandex.ru>
2024-03-06 01:18:26 +0500

    feat: lstm model to process files added

---
+++ apps/file/lstm_model/lstm_model.py
@@ -0,0 +1,74 @@
+import os
+import json
+from pathlib import Path
+
+import pandas as pd
+from pymystem3 import Mystem
+from keras.models import load_model
+from keras.utils import pad_sequences
+from keras.preprocessing.text import Tokenizer, tokenizer_from_json
+
+
+class Model:
+
+    base_dir = Path(__file__).resolve().parent.parent
+
+    model_relative_path = "best_model_LSTM10000_2.h5"
+    tokenizer_relative_path = "tokenizer_json.json"
+
+    def get_path_model(self):
+        #abs_model_path = os.path.abspath(self.model_relative_path)
+        #str(self.base_dir) + "/apps/file/lstm_model/best_model_LSTM10000_2.h5"
+        return str(self.base_dir) + "/lstm_model/best_model_LSTM10000_2.h5"
+
+    def get_path_tokenizer(self):
+        #abs_tokenizer_path = os.path.abspath(self.tokenizer_relative_path)
+        return str(self.base_dir) + "/lstm_model/tokenizer_json.json"
+
+
+def process_file_data(file_path, name_column):
+    data_comments = pd.read_excel(file_path)[name_column]
+    m = Mystem()
+    commentaries = []
+    coefficient = []
+    marks = []
+
+    for comment_index in range(len(data_comments)):
+        if type(data_comments[comment_index]) == str:
+            lemms = m.lemmatize(data_comments[comment_index])
+            prep = [word.strip(r'!?"#$%&()*+,-./:;<=>?@[\\]^_`{|}~\t\n').lower() for word in lemms if word.isalpha()]
+            sequence = tokenizer.texts_to_sequences([' '.join(prep)])
+            sequence = pad_sequences(sequence, 64)
+            score = model.predict(sequence)
+
+            if score[[0]] > 0.9:
+                commentaries.append(data_comments[comment_index])
+                marks.append('Отрицательный')
+                coefficient.append(score)
+
+            elif 0.9 > score[[0]] > 0.4:
+                commentaries.append(data_comments[comment_index])
+                marks.append('Нейтральный')
+                coefficient.append(score)
+
+            else:
+                commentaries.append(data_comments[comment_index])
+                marks.append('Положительный')
+                coefficient.append(score)
+
+    data = pd.DataFrame()
+    #headers = list(data.columns.values)
+    #data.drop(headers, axis=1, inplace=True)
+    data['Сообщение'] = commentaries
+    data['Класс'] = marks
+    data['Коэффициент'] = coefficient
+    data.to_excel(file_path)
+
+
+model_lstm = Model()
+
+model = load_model(model_lstm.get_path_model())
+
+with open(model_lstm.get_path_tokenizer()) as file:
+    data = json.load(file)
+    tokenizer = tokenizer_from_json(data)
