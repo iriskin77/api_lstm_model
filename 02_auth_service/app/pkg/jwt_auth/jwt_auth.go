3e9789640782547fa1fef64c4994fa321593dbe3
iriskin77 <talcore@yandex.ru>
2024-05-10 12:49:38 +0500

    add: mongo db

--- 02_auth_service/app/pkg/jwt_auth/jwt_auth.go
+++ 02_auth_service/app/pkg/jwt_auth/jwt_auth.go
@@ -1,1 +1,81 @@
 package jwt_auth
+
+import (
+	"errors"
+	"fmt"
+	"math/rand"
+	"time"
+
+	"github.com/dgrijalva/jwt-go"
+)
+
+type Tokens struct {
+	AccessToken  string
+	RefreshToken string
+}
+
+// TokenManager provides logic for JWT & Refresh tokens generation and parsing.
+type TokenManager interface {
+	NewJWT(userId string) (string, error)
+	Parse(accessToken string) (string, error)
+	NewRefreshToken() (string, error)
+}
+
+type Manager struct {
+	signingKey      string
+	AccessTokenTTL  time.Duration
+	RefreshTokenTTL time.Duration
+}
+
+func NewManager(signingKey string, accessTokenTTL time.Duration, refreshTokenTTL time.Duration) (*Manager, error) {
+	if signingKey == "" {
+		return nil, errors.New("empty signing key")
+	}
+
+	return &Manager{
+		signingKey:      signingKey,
+		AccessTokenTTL:  accessTokenTTL,
+		RefreshTokenTTL: refreshTokenTTL}, nil
+}
+
+func (m *Manager) NewJWT(userUUID string) (string, error) {
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.StandardClaims{
+		Subject:   userUUID,
+		ExpiresAt: time.Now().Add(m.AccessTokenTTL).Unix(),
+	})
+
+	return token.SignedString([]byte(m.signingKey))
+}
+
+func (m *Manager) Parse(accessToken string) (string, error) {
+	token, err := jwt.Parse(accessToken, func(token *jwt.Token) (i interface{}, err error) {
+		if _, ok := token.Method.(*jwt.SigningMethodHMAC); !ok {
+			return nil, fmt.Errorf("unexpected signing method: %v", token.Header["alg"])
+		}
+
+		return []byte(m.signingKey), nil
+	})
+	if err != nil {
+		return "", err
+	}
+
+	claims, ok := token.Claims.(jwt.MapClaims)
+	if !ok {
+		return "", fmt.Errorf("error get user claims from token")
+	}
+
+	return claims["sub"].(string), nil
+}
+
+func (m *Manager) NewRefreshToken() (string, error) {
+	b := make([]byte, 32)
+
+	s := rand.NewSource(time.Now().Unix())
+	r := rand.New(s)
+
+	if _, err := r.Read(b); err != nil {
+		return "", err
+	}
+
+	return fmt.Sprintf("%x", b), nil
+}
